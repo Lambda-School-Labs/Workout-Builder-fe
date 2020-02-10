f0cb534755f634f97475b1cd5b5238073c702a10
macjabeth <macbethjonathan@gmail.com>
2020-02-10 12:23:17 -0500

    add sentry configuration

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import reducer from './components/reducers/index'
+import reducer from './components/reducers/index';
 import { globalHistory } from "@reach/router";
 import './styles/index.css';
+
+Sentry.init({ dsn: process.env.REACT_APP_SENTRY_DSN });
 
 globalHistory.listen(({ location, action }) => {
   console.log({ location, action });
 });
 
 const store = createStore(reducer, applyMiddleware(thunk));
-ReactDOM.render(
-  <Provider store = {store}>
-    <App />
-  </Provider>
 
-, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
