551122f8659c668936c77c5d8a06c9b76b5613f2
macjabeth <macbethjonathan@gmail.com>
2020-02-11 18:05:37 -0500

    update tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 module.exports = {
   theme: {
-    extend: {},
-    lineHeight: {
-      middle: "0.1em"
+    fontFamily: {
+      body: ["Montserrat", "sans-serif"]
+    },
+    extend: {
+      colors: {
+        "blaze-orange": "#FD6A02",
+        "cornflower-blue": "#F7F7FA",
+        "mine-shaft": "#333333",
+        "dove-grey": "#666666",
+        silver: "#BEBEBE",
+        greyish: "#757575",
+        redish: "#FF0F0F"
+      },
+      lineHeight: {
+        middle: "0.1em"
+      },
+      fontSize: {
+        half: ".5rem"
+      }
     }
   },
   variants: {},
   plugins: []
-}
+};
