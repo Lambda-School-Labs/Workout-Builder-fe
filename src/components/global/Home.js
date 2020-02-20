187a8b25e78f7bc445c5883218dcbf8725787352
Harrison <hmseaborn@gmail.com>
2020-02-20 15:38:51 -0800

    hotfix button width

--- src/components/global/Home.js
+++ src/components/global/Home.js
@@ -2,16 +2,14 @@ import React from 'react';
 import{ Link } from "@reach/router";
 
 const Home= props => {
-  // flex flex-row justify-between w-full bg-cornflower-blue text-dove-grey lg:items-center rounded-tr-lg rounded-br-lg py-5
   return (
-
     <div className="flex flex-col justify-center items-center mt-32 sm:flex-row sm:absolutesm:m-0 sm:mt-0 sm:w-screen sm:justify-start sm:justify-between ">
       <h1 className="text-blaze-orange items-center text-4xl font-display pb-16 sm:pb-0 sm:ml-6">
       Stronger Faster
       </h1>
       <div className="flex flex-col items-center sm:flex sm:flex-row sm:items-center sm:justify-end sm:justify-between sm:mr-6">
-        <Link to ="/login"><button className ="bg-white rounded text-blaze-orange border-2 border-blaze-orange rounded font-semibold text-sm mb-6 py-2 w-40 h-10 px-4 rounded sm:mb-0 sm:mr-6 ">Login</button></Link>
-        <Link to ="/signup"><button className ="bg-blaze-orange rounded text-white font-semibold text-sm py-2 w-40 h-10 px-4 rounded">Sign Up</button></Link>
+        <Link to ="/login"><button className ="bg-white rounded text-blaze-orange border-2 border-blaze-orange rounded font-semibold text-sm mb-6 py-2 w-48 h-10 px-4 rounded sm:mb-0 sm:mr-6 ">Login</button></Link>
+        <Link to ="/signup"><button className ="bg-blaze-orange rounded text-white font-semibold text-sm py-2 w-48 h-10 px-4 rounded">Sign Up</button></Link>
       </div>
     </div>
   );
