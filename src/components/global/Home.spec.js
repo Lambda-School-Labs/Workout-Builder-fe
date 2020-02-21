6953b59100ddc2e25f77a308ff999ed1c849c451
macjabeth <macbethjonathan@gmail.com>
2020-02-21 12:09:21 -0500

    add render test for landing page
    Co-authored-by: Harrison Seaborn <hmseaborn@gmail.com>

---
+++ src/components/global/Home.spec.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Home from "./Home";
+
+test("it renders to the screen", () => {
+  const { getByText } = render(<Home />);
+  getByText("Login");
+});
