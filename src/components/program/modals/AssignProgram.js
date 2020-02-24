fe929ba245b943f2d593c819eeb8ddfb70cb8def
ngaivoronski <ngaivoronski@gmail.com>
2020-02-24 13:41:42 -0500

    bug fixes - assign modal crash and assign number updates

--- src/components/program/modals/AssignProgram.js
+++ src/components/program/modals/AssignProgram.js
@@ -10,108 +10,109 @@ const AssignProgram = (props) => {
     const [confirmModal, toggleConfirmModal] = useState(false);
 
     const closeModal = (e) => {
-      e.stopPropagation();
-      props.toggleConfirmModal(false);
-  }
+        e.stopPropagation();
+        props.ToggleAssignProgramModal(false);
+    }
 
-  function useOutsideAlerter(ref) {
-      function handleClickOutside(event) {
-          if (ref.current && !ref.current.contains(event.target)) {
-          closeModal(event);
-          }
-      }
+    function useOutsideAlerter(ref) {
+        function handleClickOutside(event) {
+            if (ref.current && !ref.current.contains(event.target)) {
+            closeModal(event);
+            }
+        }
 
-      useEffect(() => {
-          // Bind the event listener
-          document.addEventListener("mousedown", handleClickOutside);
-          return () => {
-          // Unbind the event listener on clean up
-          document.removeEventListener("mousedown", handleClickOutside);
-          };
-      });
-  }
+        useEffect(() => {
+            // Bind the event listener
+            document.addEventListener("mousedown", handleClickOutside);
+            return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+            };
+        });
+    }
 
-  const wrapperRef = useRef(null);
-  useOutsideAlerter(wrapperRef);
+    const wrapperRef = useRef(null);
+    useOutsideAlerter(wrapperRef);
 
 
-  /***** Assigning methods *****/ 
+    /***** Assigning methods *****/ 
 
-  // This is the current program for which the assign list is being displayed
-  const thisProgram = props.coach_programs.filter(program => program.id === props.program_id)[0];
+    // This is the current program for which the assign list is being displayed
+    const thisProgram = props.coach_programs.filter(program => program.id === props.program_id)[0];
 
-  const [clientList, setClientList] = useState([... thisProgram.assigned_clients]);
+    const [clientList, setClientList] = useState([... thisProgram.assigned_clients]);
 
-  const isChecked = (client_id) => {
-      // function to determine if a box should be checked for each client name
-      if (clientList.includes(client_id)) {
-          return "checked";
-      } else {
-          return "";
-      }
-  }
+    const isChecked = (client_id) => {
+        // function to determine if a box should be checked for each client name
+        if (clientList.includes(client_id)) {
+            return "checked";
+        } else {
+            return "";
+        }
+    }
 
   
-  const toggleAssign = (client_id) => {
-      // function to check / uncheck boxes
-      if (clientList.includes(client_id)) {
-          // remove client
-          let tempList = [...clientList];
-          const index = clientList.indexOf(client_id);
-          tempList.splice(index, 1);
-          setClientList(tempList);
-      } else {
-          // add client
-          let tempList = [...clientList];
-          tempList.push(client_id);
-          setClientList(tempList);
-      }
-  }
+    const toggleAssign = (client_id) => {
+        // function to check / uncheck boxes
+        if (clientList.includes(client_id)) {
+            // remove client
+            let tempList = [...clientList];
+            const index = clientList.indexOf(client_id);
+            tempList.splice(index, 1);
+            setClientList(tempList);
+        } else {
+            // add client
+            let tempList = [...clientList];
+            tempList.push(client_id);
+            setClientList(tempList);
+        }
+    }
 
-  // function to update the redux data
-  const assignToClient = (e) => {
-      const updatedProgram = {... thisProgram, assigned_clients: clientList};
-      Dispatch({ type: "UPDATE_A_PROGRAM", payload: updatedProgram });
-      toggleConfirmModal(true);
-  }
+    // function to update the redux data
+    const assignToClient = (e) => {
+        const updatedProgram = {... thisProgram, assigned_clients: clientList};
+        Dispatch({ type: "UPDATE_A_PROGRAM", payload: updatedProgram });
+        Dispatch({ type: "UPDATE" });
+        toggleConfirmModal(true);
+    }
 
-  return(
-    <div className="assign-modal">
-            <div ref={wrapperRef} className="assign-internal">
-                <div className="assign-header">
-                    <div className="assign-header-clients">Clients</div>
-                    <div className="assign-header-teams">Teams</div>
+    return(
+        <div className="assign-modal">
+                <div ref={wrapperRef} className="assign-internal">
+                    <div className="assign-header">
+                        <div className="assign-header-clients">Clients</div>
+                        <div className="assign-header-teams">Teams</div>
+                    </div>
+                    <div className="assign-clients" style={{}}>
+                        {props.coach_clients.map(client => {
+                            return(
+                                <div className="assign-clients-row" id={`assign-client-${client.id}`}>
+                                        <label class="assign-client-container">
+                                            <input type="checkbox" checked={isChecked(client.id)} onClick={() => toggleAssign(client.id)}/>
+                                            {/* checked={isChecked(client.id)} */}
+                                            {client.first_name} {client.last_name}
+                                        </label>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className="assign-button-div">
+                        <button className="assign-button" onClick={assignToClient}>Assign to client</button>
+                    </div>
+                    {confirmModal ? <AssignConfirm thisProgram={thisProgram} 
+                    confirmModal={confirmModal} toggleConfirmModal={toggleConfirmModal} {...props}/>
+                    : <div />}
                 </div>
-                <div className="assign-clients" style={{}}>
-                    {props.coach_clients.map(client => {
-                        return(
-                            <div className="assign-clients-row" id={`assign-client-${client.id}`}>
-                                    <label class="assign-client-container">
-                                        <input type="checkbox" checked={isChecked(client.id)} onClick={() => toggleAssign(client.id)}/>
-                                        {/* checked={isChecked(client.id)} */}
-                                        {client.first_name} {client.last_name}
-                                    </label>
-                            </div>
-                        )
-                    })}
-                </div>
-                <div className="assign-button-div">
-                    <button className="assign-button" onClick={assignToClient}>Assign to client</button>
-                </div>
-                {confirmModal ? <AssignConfirm thisProgram={thisProgram} 
-                confirmModal={confirmModal} toggleConfirmModal={toggleConfirmModal} {...props}/>
-                : <div />}
-            </div>
-    </div>
-  )
+        </div>
+    )
 }
 
 const mapStateToProps = state => ({
-  loggedInUser: state.loggedInUser,
-  updates: state.updates,
-  coach_clients: state.coach_clients,
-  coach_exercises: state.coach_exercises,
-  coach_programs: state.coach_programs,
+    loggedInUser: state.loggedInUser,
+    updates: state.updates,
+    coach_clients: state.coach_clients,
+    coach_exercises: state.coach_exercises,
+    coach_programs: state.coach_programs,
 });
 
 export default connect(
