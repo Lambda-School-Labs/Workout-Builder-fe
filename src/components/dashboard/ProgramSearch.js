0063542e2f716906a220619b8754931735aa3c48
macjabeth <macbethjonathan@gmail.com>
2020-02-25 13:48:53 -0500

    wrap search input in a form element

--- src/components/dashboard/ProgramSearch.js
+++ src/components/dashboard/ProgramSearch.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 const ProgramSearch = React.memo(({ query, handleQueryChange }) => {
   return (
-    <input
-      type="search"
-      name="search"
-      id="search"
-      placeholder="Search"
-      value={query}
-      onChange={handleQueryChange}
-      className="py-2 px-4 placeholder-silver font-medium text-sm"
-    />
+    <form>
+      <input
+        type="search"
+        name="search"
+        id="search"
+        placeholder="Search"
+        value={query}
+        onChange={handleQueryChange}
+        className="py-2 px-4 placeholder-silver font-medium text-sm"
+      />
+    </form>
   );
 });
 
